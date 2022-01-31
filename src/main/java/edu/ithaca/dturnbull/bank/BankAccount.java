043d2b3a8fa93e638a97466fa51d2083f0465c45
ellen6820 <echapman@ithaca.edu>
2022-01-30 21:18:02 -0500

    isAmountValid added to withdraw and constructor

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -12,10 +12,14 @@ public class BankAccount {
     public BankAccount(String email, double startingBalance){
         if (isEmailValid(email)){
             this.email = email;
-            this.balance = startingBalance;
+
         }
         else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
+        }
+
+        if(isAmountValid(startingBalance)){
+            this.balance = startingBalance;
         }
     }
 
